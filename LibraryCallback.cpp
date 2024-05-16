240ec792cda410b1549015691dfd69803d6e2bd6
marc2k3 <marc2003@live.co.uk>
2024-05-16 15:52:21 +0100

    on_library_initialised

--- LibraryCallback.cpp
+++ LibraryCallback.cpp
@@ -2,12 +2,15 @@
 
 namespace
 {
-	class LibraryCallback : public library_callback
+	class LibraryCallback : public library_callback_v2
 	{
 	public:
 		void on_items_added(metadb_handle_list_cref handles) final
 		{
-			PanelManager::get()->notify_handles(CallbackID::on_library_items_added, handles);
+			if (library_manager_v4::get()->is_initialized())
+			{
+				PanelManager::get()->notify_handles(CallbackID::on_library_items_added, handles);
+			}
 		}
 
 		void on_items_modified(metadb_handle_list_cref handles) final
@@ -17,8 +20,18 @@ namespace
 
 		void on_items_removed(metadb_handle_list_cref handles) final
 		{
-			PanelManager::get()->notify_handles(CallbackID::on_library_items_removed, handles);
+			if (library_manager_v4::get()->is_initialized())
+			{
+				PanelManager::get()->notify_handles(CallbackID::on_library_items_removed, handles);
+			}
 		}
+
+		void on_library_initialized() final
+		{
+			PanelManager::get()->notify(CallbackID::on_library_initialised);
+		}
+
+		void on_items_modified_v2(metadb_handle_list_cref, metadb_io_callback_v2_data&) final {}
 	};
 
 	FB2K_SERVICE_FACTORY(LibraryCallback);
